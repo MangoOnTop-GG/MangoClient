43c22529f5e40ccc914d0d5e988755293c4f9a53
Mango <mangoontop2@gmail.com>
2026-08-15 21:37:45 -0400

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.mango.gorillatag.menutemplate";
+        public const string Name = "Mango Client";
+        public const string Description = "Created By MangoVRYT";
+        public const string Version = "2.0.2";
+    }
+}
